232118bcab9bd30c52708244e8d618f169029a75
GonzaloGuzman <gonzalo.esteban2875@gmail.com>
2022-01-11 17:11:41 -0300

    [CHANGE] Se borran console.log

--- src/server.js
+++ src/server.js
@@ -12,12 +12,6 @@ const APP_CONTEXT_PATH = process.env.APP_CONTEXT_PATH ? process.env.APP_CONTEXT_
 const APP_PORT = process.env.APP_PORT ? process.env.APP_PORT : 9001;
 const dev = process.env.NODE_ENV !== "production";
 
-console.log("mysql",process.env.DB_HOST);
-console.log("APP_URL",APP_URL);
-console.log("APP_CONTEXT_PATH",APP_CONTEXT_PATH);
-console.log("APP_PORT",APP_PORT);
-console.log("dev",dev);
-
 const HOST_MYSQL = process.env.DB_HOST ? process.env.DB_HOST : 'locahost';
 const PORT_MYSQL = process.env.DB_PORT ? process.env.DB_PORT : 3306;
 const USER_MYSQL = process.env.DB_USER ? process.env.DB_USER : 'xxx';
@@ -31,8 +25,6 @@ const dbOptions = {
     password: PASS_MYSQL,
     database: DB_NAME
 }
-
-console.log("credenciales DB: ",dbOptions);
 
 // middlewares -------------------------------------
 app.use(myconn(mysql, dbOptions, 'single'))
